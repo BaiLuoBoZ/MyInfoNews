a4b139becddc548b5b51d584834bc7e014b42dc2
朱赞聪 <1147550511@qq.com>
2018-08-24 11:57:29 +0800

    设置session存储

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 
@@ -13,6 +16,16 @@ class Config:
     # 配置redis的ip和端口号
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+    # 指定session存储的数据库类型
+    SESSION_TYPE = "redis"
+    # 设置session存储使用的redis对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 对cookie中保存的sessionid进行加密
+    SESSION_USE_SIGNER = True
+    # 应用秘钥
+    SECRET_KEY = "j7u1TSTgxEXcjJIqrauuHirMfLZnbGAbHRVopZftM5/w0PLGduhUcVHY95dIg8So9Mip+hnl39W1N1jPgimxXA=="
+    # 设置session存储时间
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
 
 
 app = Flask(__name__)
@@ -21,13 +34,14 @@ app.config.from_object(Config)
 # 创建mysql数据库连接对象
 db = SQLAlchemy(app)
 # 创建redis数据库连接对象
-sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
+sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 初始化session存储对象
+Session(app)
 
 
 @app.route('/')
 def index():
-    sr.set("name", "zhangsan")
-    print(sr.get("name"))
+    session["name"] = "lisi"
     return "index"
 
 
